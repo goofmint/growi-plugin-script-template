504d0d25be867103c0c341ac94631e7a0cf934bc
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2024-06-28 09:55:13 +0900

    Add try/catch

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -2,6 +2,14 @@ import './Hello.css';
 
 export const helloGROWI = (Tag: React.FunctionComponent<any>): React.FunctionComponent<any> => {
   return ({ children, ...props }) => {
+    try {
+      // your code here
+      // return <>Hello, GROWI!</>;
+    }
+    catch (err) {
+      // console.error(err);
+    }
+    // Return the original component if an error occurs
     return (
       <Tag {...props}>{children}</Tag>
     );
